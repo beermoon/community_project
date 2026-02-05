ff65c7e67049517c53e2bb1a0f504bcd11f97d18
beermoon <Feathermoon0318@gmail.com>
2026-02-05 16:59:11 +0900

    add 2025/2/05

---
+++ community_project/src/main/java/kr/co/choi/community_project/CommunityProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.choi.community_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityProjectApplication.class, args);
+    }
+
+}
---
+++ community_project/src/test/java/kr/co/choi/community_project/CommunityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.choi.community_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
